455a9a98af1071f555473ef060840a1a8cdcf53b
Eito Nishikawa <ena50@sfu.ca>
2024-11-12 00:41:36 -0800

    fixed the Calories card

--- app/nutrition/components/caloreis-card.tsx
+++ app/nutrition/components/caloreis-card.tsx
@@ -4,18 +4,17 @@ import { Card } from "@/components/ui/card";
 
 export function CaloreisCard() {
   return (
-    
-      <Card className="border border-gray-200 shadow-lg h-[205px]">
-      <div className="p-4">
-       <div className="text-lg font-medium">1 Potato (148g / 5.3oz)</div>
-        <div className="text-3xl font-bold mt-2">Calories</div>
-        <div className="text-sm text-gray-500  mb-2">Amount per serving</div>
-        <div className="text-5xl font-semibold text-right ">110cal</div>
-        <hr className="my-2 border-gray-400" />
-      </div>
-    </Card>    
-    
-
+        <Card className="border border-gray-200 shadow-lg h-[205px] p-4 flex flex-col justify-center">
+          <div className="text-lg font-medium mb-2">1 Potato (148g / 5.3oz)</div>
+          <div className="flex items-center justify-between mb-2">
+            <div>
+              <div className="text-3xl font-bold">Calories</div>
+              <div className="text-sm text-gray-500">Amount per serving</div>
+            </div>
+            <div className="text-5xl font-semibold text-gray-600">110cal</div>
+          </div>
+          <hr className="my-2 border-gray-400" />
+        </Card>
 
   );
 }
