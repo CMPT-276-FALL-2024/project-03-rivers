12b2c667fc6f87e5bfc96c90b5632c1459f7c33d
Eito Nishikawa <ena50@sfu.ca>
2024-11-17 18:43:47 -0800

    test in the nutrition page

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,9 +1,53 @@
 
+"use client";
+//test
+import Image from "next/image";
+import RecipeCard from "./components/recipe-card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { CheckboxIngredients } from "./components/ingredients-selector";
+import { useState } from "react";
+
+interface Recipe {
+  id: number;
+  title: string;
+  image: string;
+}
 
 export default function Recipe() {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+    const handleRecipesFetched = (fetchedRecipes: Recipe[]) => {
+      console.log("Recipes received in page component:", fetchedRecipes); 
+      setRecipes(fetchedRecipes);
+    };
+
     return (
-        <div>
-            This is recipe page
+        <div className="container mx-auto px-4 py-8">
+            <div className="flex flex-col md:flex-row items-center justify-center mt-20 gap-6">
+                <div className="w-full md:w-1/3">
+                    <CheckboxIngredients onRecipesFetched={handleRecipesFetched} />
+                </div>
+
+                <Image 
+                    src="/recipe/arrow.png"
+                    alt="recipe image"
+                    width={100}
+                    height={68}
+                    className="hidden md:block"
+                />
+
+                <ScrollArea className="w-full md:w-2/3 h-[700px] rounded-md border p-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        {recipes.length === 0 ? (
+                            <p className="col-span-full text-center items-center justify-center text-gray-400">Select ingredients to search for recipes...</p>
+                        ) : (
+                            recipes.map((recipe) => (
+                                <RecipeCard key={recipe.id} recipe={recipe} />
+                            ))
+                        )}
+                    </div>        
+                </ScrollArea>
+            </div>
         </div>
     );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['img.spoonacular.com'],
+  },
+  /* other config options if any */
 };
 
 export default nextConfig;
+
