706e24e63c0779685ea438cfdd21c75bc10de8e7
Ryota Kato <rka147@sfu.ca>
2024-11-25 21:19:16 -0800

    Changed some words

--- app/calendar/components/event-details.tsx
+++ app/calendar/components/event-details.tsx
@@ -44,7 +44,7 @@ export function EventDetails({ event, onClose }: EventDetailsProps) {
 
       <DrawerFooter>
         <Button variant="outline" onClick={onClose}>
-          閉じる
+          Close
         </Button>
       </DrawerFooter>
     </DrawerContent>
