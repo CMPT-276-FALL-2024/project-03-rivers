7dd0906c041fa0428eac52510139d00f37f39cc5
Ryota Kato <rka147@sfu.ca>
2024-11-27 12:53:18 -0800

    deleted ldrs

--- components/menu-bar.tsx
+++ components/menu-bar.tsx
@@ -15,7 +15,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
-import 'ldrs/quantum'
+// import 'ldrs/quantum'
 // import Icon from "./icons/helix";
 // import Helix from "./icons/helix";
 
