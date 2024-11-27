cbac3bcfbb27a252e3f9239e4c3b4cc3aecbcf34
Eito Nishikawa <ena50@sfu.ca>
2024-11-26 20:16:20 -0800

    add the function to go to other page from nutrition page.

--- app/recipe/result/nutrition/page.tsx
+++ app/recipe/result/nutrition/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { Suspense, useEffect, useState } from 'react';
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { ImageCard } from './components/image-card';
 import { NutritionFactsCard } from './components/nutrition-card';
 import { CaloriesCard } from './components/calories-card';
 import { Button } from '@/components/ui/button';
-import { ArrowLeftToLine, ArrowRightFromLine, ArrowUpRight } from 'lucide-react';
+import { ArrowLeftToLine, ArrowRightFromLine } from 'lucide-react';
 
 function NutritionContent() {
     const searchParams = useSearchParams();
+    const router = useRouter();
     const recipeId = searchParams.get("id");
     const [recipeName, setRecipeName] = useState<string>("");
 
@@ -31,6 +32,14 @@ function NutritionContent() {
         fetchRecipeName();
     }, [recipeId]);
 
+    const handleBackClick = () => {
+        router.push(`/recipe/result?id=${recipeId}`);
+    };
+
+    const handleCalendarClick = () => {
+        router.push('/calendar');
+    };
+
     if (!recipeId) return <p>No recipe ID provided</p>;
 
     return (
@@ -42,20 +51,19 @@ function NutritionContent() {
                
                 <div className="relative w-full h-14 px-4">
                     <div className="absolute left-0 top-1/2 -translate-y-1/2">
-                        <Button variant="secondary" className='rounded-lg'>
+                        <Button variant="secondary" className='rounded-lg' onClick={handleBackClick}>
                             <ArrowLeftToLine />
-                         Back to previous page
+                            Back to previous page
                         </Button>
                     </div>
                     <div className="absolute right-0 top-1/2 -translate-y-1/2">
-                        <Button variant="secondary" className='rounded-lg'>
-                         Go to calendar
-                         <ArrowRightFromLine />
+                        <Button variant="secondary" className='rounded-lg' onClick={handleCalendarClick}>
+                            Go to calendar
+                            <ArrowRightFromLine />
                         </Button>
                     </div>
                 </div>
                 
-            
                 <div className="grid grid-rows-2 grid-cols-2 gap-2 h-[800px]">
                     <div className="row-span-1 col-span-1">
                         <ImageCard />
