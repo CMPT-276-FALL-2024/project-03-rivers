d871c5bf25373f6aba8f7c37cc8d496bb2191d4e
Eito Nishikawa <ena50@sfu.ca>
2024-12-02 15:25:41 -0800

    Create bun.config.js

---
+++ bun.config.js
@@ -0,0 +1,8 @@
+import { expect, test, describe, beforeEach, jest } from "bun:test";
+
+global.expect = expect;
+global.test = test;
+global.describe = describe;
+global.beforeEach = beforeEach;
+global.jest = jest;
+
