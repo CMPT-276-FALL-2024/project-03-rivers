2453c673d7d8dded0ff7c978a8bd806cbbf7b1a9
Eito Nishikawa <ena50@sfu.ca>
2024-12-02 14:46:01 -0800

    Create setup.ts

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+// Add any global test setup here
+import '@testing-library/jest-dom'
+
+// If you're using MSW for API mocking, you might want to set it up here
+// import { server } from './mocks/server'
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
+
+// Add any other global setup you need for your tests
+
