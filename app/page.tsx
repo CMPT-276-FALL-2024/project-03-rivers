b07633986b403e8008b94115f6f3a98f39da3887
Ryota Kato <rka147@sfu.ca>
2024-12-06 22:28:03 -0800

    added testimonials component and inserted them to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { HomeScroll } from "@/components/home-scroll";
+import { HomeTestimonials } from "@/components/home-testimotional";
 import { BoxIntro } from "@/components/home-title";
 import SparklesText from "@/components/ui/sparkles-text";
 import Image from "next/image";
@@ -11,6 +13,10 @@ export default function Home() {
         <BoxIntro />
         <Image src="/images/home/pic1.png" alt="arrow image" width={800} height={600} className="rounded-lg" >
         </Image>
+      </div>
+      <div className="mt-16">
+        <HomeTestimonials />
+        {/* <HomeScroll /> */}
       </div>
       
     </div>
